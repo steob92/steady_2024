d7b6c4d9ea1b67317f64ecc7b5b4c948b037839c
Ste O'Brien <stephan.obrien@mcgill.ca>
2024-05-14 13:48:37 -0400

    implement div

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-from steady_calculations.math_operations import add
+from steady_calculations.math_operations import add, div
 
 x = 13.4
 y = 23.4
 
 print("Hello, World!")
 print(add(x,y))
+print(div(x,y))

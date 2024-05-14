2750f2a487514aa985c79a45f6ca829ee6698a59
Ste O'Brien <stephan.obrien@mcgill.ca>
2024-05-14 14:07:20 -0400

    Adding doc strings

--- steady_calculations/math_operations.py
+++ steady_calculations/math_operations.py
@@ -1,8 +1,35 @@
 def add(x, y):
+    """function to add x and y
+
+    Args:
+        x : float   arguement 1
+        y : float   arguement 2
+    
+    Return:
+        x + y
+    """
     return x + y
 
 def div(x, y):
+    """function to divie x by y
+
+    Args:
+        x : float   arguement 1
+        y : float   arguement 2
+    
+    Return:
+        x / y
+    """
     return x/y
 
 def mult(x, y):
+    """function to multiple x by y
+
+    Args:
+        x : float   arguement 1
+        y : float   arguement 2
+    
+    Return:
+        x * y
+    """
     return x * y
